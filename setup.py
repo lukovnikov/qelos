8003f9c778a1b94f77160ebb827ce207d759c093
lukovnikov <lukovnikov@outlook.com>
2017-08-11 16:01:38 +0200

    * mnist script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="qelos",
+      description="qelos",
+      author="ldgn",
+      author_email="ldgn@ldgn.io",
+      install_requires=["torch"],
+      packages=find_packages(),
+      )
