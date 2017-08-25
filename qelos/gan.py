88c19505f83f787aa32710a59853b1bb0c2e8fcf
lukovnikov <lukovnikov@outlook.com>
2017-08-25 21:07:06 +0200

    * added GANs

--- qelos/gan.py
+++ qelos/gan.py
@@ -111,7 +111,7 @@ class GANTrainer(object):
                     interp_alpha = real.data.new(num_examples, 1)
                     interp_alpha.uniform_(0, 1)
                     interp_points = interp_alpha * real.data + (1 - interp_alpha) * fake.data
-                    grad_points = Variable(interp_points.clone())
+                    grad_points = interp_points.clone()
                     interp_points = Variable(interp_points, requires_grad=True)
                     errD_interp_vec = netD(interp_points)
                     errD_gradient, = torch.autograd.grad(errD_interp_vec.sum(),
