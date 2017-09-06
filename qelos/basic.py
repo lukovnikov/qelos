* fixed tests * LNormDist sign reversed?

qelos/basic.py
@@ -333,7 +333,7 @@ class LNormDistance(Distance):
     def forward(self, data, crit):      # (batsize, [lseqlen,] dim), (batsize, [rseqlen,], dim)
         if data.dim() == 3 and crit.dim() == 2:
             crit = crit.unsqueeze(1)
-        if data.dim() == 3 and crit.dim() == 3:
+        elif data.dim() == 3 and crit.dim() == 3:
             data = data.unsqueeze(2)
             crit = crit.unsqueeze(1)
         temp = torch.abs(data - crit)

qelos/rnn.py
@@ -942,7 +942,10 @@ class RecurrentWrapper(Recurrent, nn.Module):
         for ye in y:
             ye = ye.view(batsize, seqlen, *ye.size()[1:])
             yo.append(ye)
-        return tuple(yo)
+        if len(yo) == 1:
+            return yo[0]
+        else:
+            return tuple(yo)
 
 
 class LastTimestepGetter(nn.Module):

qelos/scripts/webqa/seq2seq.py
@@ -2,6 +2,7 @@ import qelos as q
 from qelos.scripts.webqa.load import load_all
 import torch
 from torch import nn
+import sys
 
 
 def make_encoder(src_emb, embdim=100, dim=100, **kw):
@@ -80,8 +81,8 @@ def run(lr=0.1,
     tt.tock("loaded data and rep")
     tt.tick("making data loaders")
     # train/valid/test split:
-    train_questions, test_questions = question_sm.matrix[:tx_sep], question_sm[tx_sep:]
-    train_queries, test_queries = query_sm.matrix[:tx_sep], query_sm[tx_sep:]
+    train_questions, test_questions = question_sm.matrix[:tx_sep], question_sm.matrix[tx_sep:]
+    train_queries, test_queries = query_sm.matrix[:tx_sep], query_sm.matrix[tx_sep:]
     train_vnt, test_vnt = vnt_mat[:tx_sep], vnt_mat[tx_sep:]
 
     validportion = .2
@@ -108,7 +109,13 @@ def run(lr=0.1,
     losses = q.lossarray(q.SeqNLLLoss(), q.SeqAccuracy(), q.SeqElemAccuracy())
     validlosses = q.lossarray(q.SeqNLLLoss(), q.SeqAccuracy(), q.SeqElemAccuracy())
 
-    optimizer = torch.optim.Adadelta(m.parameters(), lr=lr, weight_decay=wreg)
+    params = []
+    for param in m.parameters():
+        if param.requires_grad:
+            params.append(param)
+    optimizer = torch.optim.Adadelta(params, lr=lr, weight_decay=wreg)
+
+    sys.exit()
 
     # train
     q.train(m).cuda(cuda).train_on(train_dataloader, losses)\

qelos/seq.py
@@ -119,11 +119,12 @@ class Decoder(nn.Module):
         y_t = None
         for t in range(maxtime):
             #x_t = [x_e[:, t] if x_e.sequence else x_e for x_e in x]
-            x_t = self.block._get_inputs_t(t=t, x=x, y_t=y_t)        # let the Rec definition decide what to input
+            x_t, x_t_kw = self.block._get_inputs_t(t=t, x=x, xkw=kw, y_t=y_t)        # let the Rec definition decide what to input
             if not issequence(x_t):
                 x_t = [x_t]
             x_t = tuple(x_t)
-            blockret = self.block(*x_t, t=t)
+            x_t_kw["t"] = t
+            blockret = self.block(*x_t, **x_t_kw)
             if not issequence(blockret):
                 blockret = [blockret]
             y_t = blockret
@@ -307,13 +308,13 @@ class DecoderCell(RecStatefulContainer):
         assert(isinstance(reccable, RecStateful))
         self.core = reccable
 
-    def _get_inputs_t(self, t, x, y_t):
+    def _get_inputs_t(self, t=None, x=None, xkw=None, y_t=None):
         if self._inputs_t_getter is None:
-            return self.get_inputs_t(t, x, y_t)
+            return self.get_inputs_t(t=t, x=x, xkw=xkw, y_t=y_t)
         else:
-            return self._inputs_t_getter(t, x, y_t)
+            return self._inputs_t_getter(t=t, x=x, xkw=xkw, y_t=y_t)
 
-    def get_inputs_t(self, t, x, y_t):
+    def get_inputs_t(self, t=None, x=None, xkw=None, y_t=None):
         """
         Make the inputs to cell from timestep, inputs to decoder and previous outputs of cell.
         Called before every call to .forward() and must compute all arguments for .forward() for given timestep.
@@ -322,11 +323,12 @@ class DecoderCell(RecStatefulContainer):
         enable official teacher forcing support).
         This method could also be used for computing dynamic contexts (attention)
         :param t: timestep (integer)
-        :param x: original inputs to decoder (list of tensors)
+        :param x: original arguments to decoder (list of tensors)
+        :param xkw: original kwargs to decoder
         :param y_t: previous outputs of this cell (list of tensors or None). If None, no previous outputs have been output yet
-        :return: actual inputs to .forward() of this decoder cell (list of tensors)
+        :return: actual (inputs, kwinpts) to .forward() of this decoder cell (list of tensors)
         """
-        return x[0][:, t]
+        return x[0][:, t], {"t": t}
 
     def set_inputs_t_getter(self, callabla):
         self._inputs_t_getter = callabla
@@ -367,8 +369,8 @@ class ContextDecoderCell(DecoderCell):
         ret = self.core(inp)
         return ret
 
-    def get_inputs_t(self, t, x, y_t):
-        return x[0][:, t], x[1]
+    def get_inputs_t(self, t=None, x=None, xkw=None, y_t=None):
+        return (x[0][:, t], x[1]), {}
 
 
 class AttentionDecoderCell(DecoderCell):
@@ -434,7 +436,7 @@ class AttentionDecoderCell(DecoderCell):
 
 
     # region implement DecoderCell signature
-    def forward(self, x_t, ctx, ctxmask=None, t=None, **kw):
+    def forward(self, x_t, ctx, ctxmask=None, t=None, outmask_t=None, **kw):
         """
         :param x_t:     (batsize,...) input for current timestep
         :param ctx:     (batsize, inpseqlen, dim) whole context
@@ -461,7 +463,10 @@ class AttentionDecoderCell(DecoderCell):
         if self.ctx_to_smo:     cat_to_smo.append(ctx_t)
         if self.decinp_to_smo:  cat_to_smo.append(x_t_emb)
         smoinp_t = torch.cat(cat_to_smo, 1) if len(cat_to_smo) > 1 else cat_to_smo[0]
-        y_t = self.smo(smoinp_t) if self.smo is not None else smoinp_t
+        smokw = {}
+        if outmask_t is not None:
+            smokw["outmask"] = outmask_t
+        y_t = self.smo(smoinp_t, **kw) if self.smo is not None else smoinp_t
         # returns
         ret = tuple()
         if self.return_out:
@@ -497,11 +502,14 @@ class AttentionDecoderCell(DecoderCell):
         res = self.attention.attcon(ctx, att_weights)
         return res, att_weights
 
-    def get_inputs_t(self, t, x, y_t):      # TODO implement teacher forcing
-        if len(x) == 2:     # no mask
-            return x[0][:, t], x[1]
-        elif len(x) == 3:   # with mask
-            return x[0][:, t], x[1], x[2]
+    def get_inputs_t(self, t=None, x=None, xkw=None, y_t=None):      # TODO implement teacher forcing
+        outargs = (x[0][:, t], x[1])    # (prev_token, ctx)
+        outkwargs = {"t": t}
+        if "ctxmask" in xkw:        # copy over ctxmask (shared over decoder steps)
+            outkwargs["ctxmask"] = xkw["ctxmask"]
+        if "outmask" in xkw:        # slice out the time from outmask
+            outkwargs["outmask_t"] = xkw["outmask"][:, t]
+        return outargs, outkwargs
     # endregion
 
     # region RecStatefulContainer signature

qelos/word.py
@@ -314,6 +314,16 @@ class PretrainedWordVec(object):
         else:
             W = self.loadcache[path][0]
 
+        # load words
+        if path not in self.loadcache:
+            words = pkl.load(open(path+".words"))
+        else:
+            words = self.loadcache[path][1]
+
+        # cache
+        if self.useloadcache:
+            self.loadcache[path] = (W, words)
+
         # adapt
         if indim is not None:
             W = W[:indim, :]
@@ -323,16 +333,6 @@ class PretrainedWordVec(object):
             W = np.concatenate([np.zeros_like(W[0, :])[np.newaxis, :], W], axis=0)
         tt.tock("vectors loaded")
         tt.tick()
-
-        # load words
-        if path not in self.loadcache:
-            words = pkl.load(open(path+".words"))
-        else:
-            words = self.loadcache[path]
-
-        # cache
-        if self.useloadcache:
-            self.loadcache[path] = (W, words)
 
         # dictionary
         D = OrderedDict()

test/test_basic.py
@@ -458,7 +458,7 @@ class TestDistance(TestCase):
             npd[i] = x
         print(d)
         print(npd)
-        self.assertTrue(np.allclose(-d, npd))
+        self.assertTrue(np.allclose(d, npd))
 
     def test_lnorm_same_as_numpy_3D2D(self):
         a = Variable(torch.FloatTensor(np.random.random((20, 5, 2))))
@@ -474,7 +474,7 @@ class TestDistance(TestCase):
                 npd[i, j] = x
         print(d)
         print(npd)
-        self.assertTrue(np.allclose(-d, npd))
+        self.assertTrue(np.allclose(d, npd))
 
     def test_lnorm_same_as_numpy_3D3D(self):
         a = Variable(torch.FloatTensor(np.random.random((20, 5, 2))))
@@ -491,7 +491,7 @@ class TestDistance(TestCase):
                     npd[i, j, k] = x
         print(d[0])
         print(npd[0])
-        self.assertTrue(np.allclose(-d, npd))
+        self.assertTrue(np.allclose(d, npd))
 
     def test_lnorm_linear_sum_assignment(self):
         from scipy import optimize as spopt

test/test_word.py
@@ -219,7 +219,8 @@ class TestPretrainedWordLinout(TestCase):
 
     def test_loaded(self):
         thevector = self.glove % "the"
-        truevector = np.asarray([  4.18000013e-01,   2.49679998e-01,  -4.12420005e-01,
+        truevector = np.asarray([
+         4.18000013e-01,   2.49679998e-01,  -4.12420005e-01,
          1.21699996e-01,   3.45270008e-01,  -4.44569997e-02,
         -4.96879995e-01,  -1.78619996e-01,  -6.60229998e-04,
         -6.56599998e-01,   2.78430015e-01,  -1.47670001e-01,