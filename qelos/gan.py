9195b4a061d36440c5118149853516d4004e33b9
lukovnikov <lukovnikov@outlook.com>
2017-09-10 23:37:57 +0200

    * acha gan fix

--- qelos/gan.py
+++ qelos/gan.py
@@ -169,7 +169,7 @@ class GANTrainer(object):
                     # get closest real point
                     _, closest_real = torch.max(distmat, 1)
                     closest_real_points = torch.index_select(real, 0, closest_real)
-                    interp_alpha = real.data.new(num_examples, 1)
+                    interp_alpha = real.data.new(num_examples, 0)
                     interp_alpha.uniform_(0, 1)
                     interp_points = interp_alpha * real.data \
                         + (1 - interp_alpha) * closest_real_points.data
