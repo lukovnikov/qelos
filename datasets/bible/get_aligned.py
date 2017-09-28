7270de203c0eed152e9beb70eeeecb34379b4e48
lukovnikov <lukovnikov@outlook.com>
2017-09-28 21:18:28 +0200

    * started adding bible * seq2seq qa eval fix

---
+++ datasets/bible/get_aligned.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+import re, lxml
+from lxml import etree
+
+
+# LOAD
+
+languages = ["English", "Dutch"]
+
+verses = [[] for language in languages]
+supre = re.compile("<sup[^<]+</sup>([^<]+)")
+
+hist = []
+for i, language in enumerate(languages):
+    with open(language + ".xml") as f:
+        tree = etree.parse(f)
+        segments = tree.findall(".//seg")
+        for segment in segments:
+            if segment.get("type") == "verse":
+                _, book, part, verse = segment.get("id").split(".")
+                length = 1
+                if segment.text is None:
+                    text = supre.match(etree.tostring(segment[0])).group(1)
+                    length = segment[0].text.strip().split("-")
+                    length = int(length[1]) - int(length[0])
+                else:
+                    text = segment.text.strip()
+                verses[i].append((book, int(part), (int(verse), length), text))
+
+print(len(verses[0]), len(verses[1]))
+
+
+# ALIGN
+aligned = []
+
+i = 0
+j = 0
+lverses = verses[0]
+rverses = verses[1]
+
+l, r = "", ""
+lc, rc = 0, 0
+i = 0
+j = 0
+previ = 0
+prevj = 0
+prevlverse = None
+prevrverse = None
+while i < len(verses[0]) and j < len(verses[1]):
+    lbook, lpart, (lverse, llen), ltext = lverses[i]
+    rbook, rpart, (rverse, rlen), rtext = rverses[j]
+    if lbook == rbook and lpart == rpart and lverse == rverse:
+        if previ < i:
+            l = ltext
+        else:
+            l += ltext
+        if prevj < j:
+            r = rtext
+        else:
+            r += rtext
+    if llen == 1 and rlen == 1:
+        aligned.append((l, r))
+        l, r = "", ""
+        i += 1
+        j += 1
+    elif llen > 1:
+        j += 1
+    previ = i
+    prevj = j
