d3190647180fde3b7f967f09b208f874fd391bdf
lukovnikov <lukovnikov@outlook.com>
2017-10-16 20:49:12 +0200

    * searngan first version ready

--- qelos/scripts/seqgan/searngan.py
+++ qelos/scripts/seqgan/searngan.py
@@ -216,8 +216,8 @@ def makenets(vocsize, embdim, gendim, discdim, startsym,
 
     discnet = q.RecurrentStack(
         nn.Embedding(vocsize, embdim),
-        # q.GRULayer(embdim, discdim),
-        q.GRUCell(embdim, discdim, use_cudnn_cell=True).to_layer(),
+        q.GRULayer(embdim, discdim),
+        # q.GRUCell(embdim, discdim, use_cudnn_cell=True).to_layer(),
     )
 
 
