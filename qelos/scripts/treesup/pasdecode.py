f88258e36a31cf89937ec8fd9496fc9a4973d54c
lukovnikov <lukovnikov@outlook.com>
2017-12-05 20:43:54 +0100

    * pasdecode bf and oracle seq2seq

--- qelos/scripts/treesup/pasdecode.py
+++ qelos/scripts/treesup/pasdecode.py
@@ -966,6 +966,6 @@ def run_seq2seq_oracle(lr=OPT_LR,
 if __name__ == "__main__":
     print("pytorch version: {}".format(torch.version.__version__))
     ### q.argprun(run_seq2seq_teacher_forced)
-    q.argprun(run_seq2seq_teacher_forced_structured_output_tokens)
-    # q.argprun(run_seq2seq_oracle)
+    # q.argprun(run_seq2seq_teacher_forced_structured_output_tokens)
+    q.argprun(run_seq2seq_oracle)
     # q.argprun(run)
