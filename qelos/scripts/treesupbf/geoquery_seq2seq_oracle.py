0b440203a08fe2f8af5c9d36d7a7c4c4f6b4eb50
lukovnikov <lukovnikov@outlook.com>
2018-02-02 17:55:40 +0100

    separate runner scripts

---
+++ qelos/scripts/treesupbf/geoquery_seq2seq_oracle.py
@@ -0,0 +1,5 @@
+from qelos.scripts.treesupbf.geoquery_decode import run_seq2seq_oracle as runf
+import qelos as q
+
+if __name__ == "__main__":
+    q.argprun(runf)
---
+++ qelos/scripts/treesupbf/geoquery_seq2tree_tf.py
@@ -0,0 +1,5 @@
+from qelos.scripts.treesupbf.geoquery_decode import run_seq2tree_tf as runf
+import qelos as q
+
+if __name__ == "__main__":
+    q.argprun(runf)
