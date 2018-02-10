68209ec681f84ab5b02b132a486f35784e884800
lukovnikov <lukovnikov@outlook.com>
2018-02-10 23:33:25 +0100

    wikisql * chained multilinout for bf

--- qelos/scripts/treesupbf/wikisql.py
+++ qelos/scripts/treesupbf/wikisql.py
@@ -8,6 +8,7 @@ import codecs
 import numpy as np
 from collections import OrderedDict
 import pickle as pkl
+from qelos.scripts.treesupbf.trees import Node, NodeTracker, GroupTracker
 
 
 # region DATA
@@ -142,7 +143,10 @@ def make_example(line, alltables):
 
         condl = u"<COND> COL{} OP{} <VAL> {} <ENDVAL>".format(cond[0], cond[1], condval)
         sql_wheres.append(condl)
-    sql = u"<SELECT> {} <WHERE> {}".format(sql_select, u" ".join(sql_wheres))
+    if len(sql_wheres) > 0:
+        sql = u"<QUERY> <SELECT> {} <WHERE> {}".format(sql_select, u" ".join(sql_wheres))
+    else:
+        sql = u"<QUERY> <SELECT> {}".format(sql_select)
     ret = u"{}\t{}\t{}".format(question, sql, u"\t".join(column_names))
     return ret
 # endregion
@@ -601,6 +605,211 @@ def make_out_lin(dim, ism, osm, psm, csm, inpbaseemb=None, colbaseemb=None):
 
 # endregion
 
+# region SQL TREES
+class SqlNode(Node):
+    name2ctrl = {
+        "<SELECT>": "A",
+        "<WHERE>":  "A",
+        "<COND>":   "A",
+        "COL\d+":   "NC",
+        "AGG\d+":   "NC",
+        "OP\d+":    "NC",
+        "<VAL>":    "A",
+        "<ENDVAL>": "NC",
+        "UWID\d+":  "NC",
+    }
+
+    def __init__(self, name, order=None, children=tuple(), **kw):
+        super(SqlNode, self).__init__(name, order=order, children=children, **kw)
+
+    @classmethod
+    def parse_sql(cls, inp, _rec_arg=None, _toprec=True, _ret_remainder=False):
+        if len(inp) == 0:
+            return []
+        tokens = inp
+        parent = _rec_arg
+        if _toprec:
+            tokens = tokens.replace("  ", " ").strip().split()
+        head = tokens[0]
+        tail = tokens[1:]
+
+        siblings = []
+        jumpers = {"<SELECT>": {"<QUERY>"},
+                   "<WHERE>": {"<QUERY>"},
+                   "<COND>": {"<WHERE>"},
+                   "<VAL>": {"<COND>"},}
+        while True:
+            if head == "<QUERY>":
+                children, tail = cls.parse_sql(tail, _rec_arg=head, _toprec=False)
+                ret = SqlNode(head, children=children)
+                return ret
+            elif head in jumpers:
+                if _rec_arg in jumpers[head]:
+                    children, tail = cls.parse_sql(tail, _rec_arg=head, _toprec=False)
+                    if head == "<VAL>":
+                        for i, child in enumerate(children):
+                            child.order = i
+                    node = SqlNode(head, children=children)
+                    siblings.append(node)
+                    if len(tail) > 0:
+                        head, tail = tail[0], tail[1:]
+                    else:
+                        return siblings, tail
+                else:
+                    return siblings, [head] + tail
+            else:
+                node = SqlNode(head)
+                siblings.append(node)
+                if len(tail) > 0:
+                    head, tail = tail[0], tail[1:]
+                else:
+                    return siblings, tail
+        return siblings, tail
+
+    @classmethod
+    def parse(cls, inp, _rec_arg=None, _toprec=True, _ret_remainder=False):
+        tokens = inp
+        if _toprec:
+            tokens = tokens.replace("  ", " ").strip().split()
+            for i in range(len(tokens)):
+                splits = tokens[i].split("*")
+                if len(splits) == 1:
+                    token, suffix = splits[0], ""
+                else:
+                    token, suffix = splits[0], "*"+splits[1]
+                if token not in "<QUERY> <SELECT> <WHERE> <COND> <VAL>".split():
+                    suffix = "*NC" + suffix
+                tokens[i] = token + suffix
+            ret = super(SqlNode, cls).parse(" ".join(tokens), _rec_arg=None, _toprec=True)
+            return ret
+        else:
+            return super(SqlNode, cls).parse(tokens, _rec_arg=_rec_arg, _toprec=_toprec)
+
+
+def make_tracker(osm):
+    tt = q.ticktock("tree tracker maker"); tt.tick("flushing trees"); trees = []
+    for i in range(len(osm.matrix)):
+        tree = SqlNode.parse_sql(osm[i])
+        trees.append(tree)
+    xD = {}
+    xD.update(osm.D)
+    xD.update(dict([(k+"*LS", v+len(osm.D)) for k, v in osm.D.items()]))
+    tracker = SqlGroupTracker(trees, osm.D, xD)
+
+    if True:    # TEST
+        vnt = tracker.get_valid_next(0)
+        tracker.update(0, list(vnt)[0])
+        vnt = tracker.get_valid_next(0)
+
+    tt.tock("trees flushed")
+    return tracker
+
+
+class SqlGroupTracker(object):
+    def __init__(self, trackables, coreD, outD):
+        super(SqlGroupTracker, self).__init__()
+        self.trackables = trackables
+        self.D = outD
+        self.coreD = coreD
+        self.rD = {v: k for k, v in self.D.items()}
+        self.trackers = []
+        for xe in self.trackables:
+            tracker = xe.track()
+            self.trackers.append(tracker)
+
+    def get_valid_next(self, eid):
+        tracker = self.trackers[eid]
+        nvt = tracker._nvt
+        if len(nvt) == 0:
+            nvt = {u"<MASK>"}
+        _nvt = set()
+        for x in nvt:
+            x = x.replace(u"*NC", u"")
+            _nvt.add(x)
+        nvt = map(lambda x: self.D[x], _nvt)
+        return nvt
+
+    def update(self, eid, x, altx=None):
+        tracker = self.trackers[eid]
+        nvt = tracker._nvt
+        if len(nvt) == 0:
+            pass
+        else:
+            x = self.rD[x]
+            xsplits = x.split(u"*")
+            core, suffix = xsplits[0], u""
+            if len(xsplits) > 1:
+                suffix = xsplits[1]
+            if core not in u"<QUERY> <SELECT> <WHERE> <COND> <VAL>".split():
+                suffix = u"*NC" + u"*" + suffix
+            x = core + u"*" + suffix
+            tracker.nxt(x)
+
+    def is_terminated(self, eid):
+        return self.trackers[eid].is_terminated()
+
+    def reset(self):
+        for tracker in self.trackers:
+            tracker.reset()
+
+
+def make_multilinout(lindim, baselinout, coreD, outD, tie_weights=False, chained=False, ttt=None):
+    ttt = q.ticktock("linout test") if ttt is None else ttt
+
+    for k, v in sorted(outD.items(), key=lambda (x, y): y):
+        ksplits = k.split("*")
+        if len(ksplits) == 2:
+            assert(ksplits[1] == "LS")
+            assert(coreD[ksplits[0]] == (v - len(coreD)))
+        else:
+            assert(coreD[ksplits[0]] == v)
+    assert(baselinout.D == coreD)
+
+    class StrucSMO(torch.nn.Module):
+        def __init__(self, indim, corelinout, worddic=None, chained=True, **kw):
+            super(StrucSMO, self).__init__(**kw)
+            self.dim = indim
+            self.D = worddic
+            self.chained = chained
+            self.corelinout = corelinout
+
+            annD = {"A": 0, "LS": 1}
+            self.annout = q.WordLinout(self.dim + 2, worddic=annD, bias=False)
+
+            self.coreemb = q.WordEmb(self.dim, worddic=coreD)
+            self.coreemb.embedding.weight.data.fill_(0)
+
+            appendix = torch.zeros(1, len(coreD), 2)
+            if self.chained:
+                appendix[:, :, 1] = 1.    # LS
+            self.appendix = q.val(appendix).v
+
+        def forward(self, x):
+            # predict core:
+            coreprobs = self.corelinout(x)      # (batsize, coreD)
+            coreprobs = torch.nn.LogSoftmax(1)(coreprobs).unsqueeze(2)
+
+            # prepare predict LS
+            appendix = self.appendix.repeat(x.size(0), 1, 1)
+            addition = self.coreemb.embedding.weight.unsqueeze(0)
+            if not self.chained:
+                addition = q.var(torch.zeros(addition.size())).cuda(addition).v
+            predvec = torch.cat([appendix, x.unsqueeze(1) + addition], 2)
+
+            # predict LS
+            ctrlprobs = self.annout(predvec)
+            ctrlprobs = torch.nn.LogSoftmax(2)(ctrlprobs)
+
+            # merge and flatten
+            allprobs = coreprobs + ctrlprobs
+            outprobs = torch.cat([allprobs[:, :, 0], allprobs[:, :, 1]], 1)
+            return outprobs
+
+    ret = StrucSMO(lindim, baselinout, worddic=outD, chained=chained)
+    # TODO TEST
+    return ret
+# endregion
+
 
 def run_seq2seq_tf(lr=0.1, batsize=5, epochs=100,
                    inpembdim=50, outembdim=50, outlindim=50,
@@ -616,6 +825,9 @@ def run_seq2seq_tf(lr=0.1, batsize=5, epochs=100,
     tt = q.ticktock("script")
     ism, osm, csm, psm, splits, e2cn = load_matrices()
     psm._matrix = psm.matrix * (psm.matrix != psm.D["<RARE>"])      # ASSUMES there are no real <RARE> words in psm
+
+    tracker = make_tracker(osm)     # TODO: only for bf
+
     devstart, teststart = splits
     eids = np.arange(0, len(ism), dtype="int64")
 
@@ -623,12 +835,16 @@ def run_seq2seq_tf(lr=0.1, batsize=5, epochs=100,
     outemb = make_out_emb(outembdim, osm, psm, csm, inpbaseemb=inpbaseemb)
     outlin = make_out_lin(outlindim, ism, osm, psm, csm)
 
+    # TODO: below is for bf-based scripts, move
+    mlinout = make_multilinout(outlindim, outlin, tracker.coreD, tracker.D, chained=True)
+
     class DummyModel(torch.nn.Module):
         def __init__(self):
             super(DummyModel, self).__init__()
             self.inpemb = inpemb
             self.outemb = outemb
             self.outlin = outlin
+            self.mlinout = mlinout      # TODO: only for bf
 
         def forward(self, inpseq, outseq, inpseqmaps, colnames):
             self.inpemb.prepare(inpseqmaps)
@@ -641,16 +857,14 @@ def run_seq2seq_tf(lr=0.1, batsize=5, epochs=100,
 
             testvec = q.var(torch.randn(5, outlindim)).cuda(inpseq).v
 
-            test_scores = self.outlin(testvec)
+            test_scores = self.mlinout(testvec)
             return None
 
+    m = DummyModel()
 
 
     traindata = [ism.matrix[:devstart], osm.matrix[:devstart], psm.matrix[:devstart], e2cn[:devstart]]
-
     trainloader = q.dataload(*traindata, batch_size=batsize, shuffle=True)
-
-    m = DummyModel()
 
     losses = q.lossarray(q.SeqCrossEntropyLoss(ignore_index=0))
 
@@ -673,6 +887,14 @@ def run_seq2seq_tf(lr=0.1, batsize=5, epochs=100,
 
 
 if __name__ == "__main__":
+    # q.argprun(prepare_data)
     # q.argprun(create_mats)
     # q.argprun(load_matrices)
     q.argprun(run_seq2seq_tf)
+    # tree = SqlNode.parse_sql("<SELECT> AGG0 COL5 <WHERE> <COND> COL3 OP0 <VAL> UWID1 UWID2 <ENDVAL> <COND> COL1 OP1 <VAL> UWID1 UWID2 UWID3 <ENDVAL>")
+    # print(tree.pptree())
+    # treestr = tree.pp()
+    # treestr = treestr.replace("*NC", "")
+    # print(treestr)
+    # retree = SqlNode.parse(treestr)
+    # print(retree)
