28dcf3dc24b93f9e7bca47ac59e60b174adf3180
lukovnikov <lukovnikov@outlook.com>
2018-04-10 17:50:47 +0200

    * added select col first

--- qelos/scripts/treesupbf/wikisql_clean.py
+++ qelos/scripts/treesupbf/wikisql_clean.py
@@ -1758,7 +1758,7 @@ def run_seq2seq_tf(lr=0.001, batsize=100, epochs=100,
     osm = reorder_tf(osm, reordermode=reorder)
     if selectcolfirst:
         osm = reorder_select(osm)
-    q.embed()
+    # q.embed()
     # splits
     if test:    devstart, teststart, batsize = 200, 250, 50
     datamats = [ism.matrix, osm.matrix, gwids.matrix, e2cn]
